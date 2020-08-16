07a9ef3d9572038c54428a531728ae5ac718a451
matgcabrera <68969927+matgcabrera@users.noreply.github.com>
2020-08-16 18:42:06 -0300

    entrega 1 en progreso
    punto json completado, login en progreso (entrega 1)

--- js/login.js
+++ js/login.js
@@ -4,3 +4,37 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+var user = document.forms["form-login"]["usuario"].value;
+var passwd = document.forms["form-login"]["password"].value;
+var error_user = document.getElementById("error-user");
+var error_passwd = document.getElementById("error-passwd");
+
+function validarLogin(value, name){
+    if (value == "" || value == null){
+        document.getElementById("error-login").innerHTML = "Debe ingresar su " + name;
+        error_user.style.display = "block";
+        return false;
+    }
+}
+
+function validarButton() {
+    if (user == "" || user == null) {
+        document.getElementById("error-login").innerHTML = "Debe ingresar su usuario";
+        error_user.style.display = "block";
+        return false;
+    }
+
+    if (passwd == "" || passwd == null){
+        document.getElementById("error-login").innerHTML = "Debe ingresar su contraseña";
+        alert("Debe ingresar su contraseña");
+        return false;
+    }
+}
+
+function validarForm(){
+    if (user.lenght == 0) {
+        error_user.style.display = "block";
+        user.focus();
+    }
+}
--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
+var productsArray = [];
+
+function showProductList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action" onclick="call` + product.name + `()">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    </div>
+                <p>` + product.description +`</p>
+                <p>` + product.cost +` ` + product.currency +`
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("products-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data;
+            showProductList(productsArray);
+        }
+    });
 });
