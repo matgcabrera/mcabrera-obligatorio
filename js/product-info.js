c5a5685e9f3b3ccf58a3d7348cc22bb7cb71d241
matgcabrera <68969927+matgcabrera@users.noreply.github.com>
2020-09-18 09:56:31 -0300

    4.0.1
    cambios en estilo al dropdown de usuario

--- js/product-info.js
+++ js/product-info.js
@@ -25,7 +25,7 @@ function showImagesGallery(array) {
 function showComments(array) {
 
     let htmlContentToAppend = "";
-    
+
 
     //Leo score y lo convierto en estrellas ascii
 
@@ -33,20 +33,20 @@ function showComments(array) {
         let comment = array[i];
         let starTreatment = "";
 
-        for (let a = 0; a < 5; a++){
-            if(comment.score>a){
+        for (let a = 0; a < 5; a++) {
+            if (comment.score > a) {
                 starTreatment += `<span class="fa fa-star checked"></span>`;
             } else {
                 starTreatment += `<span class="fa fa-star"></span>`;
             }
 
         }
-        htmlContentToAppend+= `
+        htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
           <div class="row">
             <div class="col">
               <div class="d-flex w-90 justify-content-between">
-                <h4 class="mb-1">`+ comment.user + " - " + starTreatment +`</h4>
+                <h4 class="mb-1">`+ comment.user + " - " + starTreatment + `</h4>
                 <small class="text-muted">` + comment.dateTime + `</small>
               </div>
               <p>"` + comment.description + `"</p>
@@ -59,56 +59,58 @@ function showComments(array) {
     }
 }
 
-/*function searchProducts(array) {
+function showRelatedProducts(array) {
     let htmlContentToAppend = "";
 
-    for (let i=0; i<array.lenght; i++){
+    for (let i = 0; i < array.lenght; i++) {
         let relatedProd = array[i];
-        
-        for(a=0; a<relatedProductstoLoad; a++){
-            if (relatedProd === relatedProductstoLoad){
+
+        for (a = 0; a < relatedProductstoLoad; a++) {
+            if (relatedProd === relatedProductstoLoad) {
                 document.getElementById("relatedProducts").innerHTML = i;
+                alert(i);
             }
         }
 
-        }
-    } 
-} */
-
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            product = resultObj.data;
-
-            let productNameHTML = document.getElementById("productName");
-            let productDescriptionHTML = document.getElementById("productDescription");
-            let productCostHTML = document.getElementById("productCost");
-            let vendidosCountHTML = document.getElementById("vendidosCount");
-            let productCategoryHTML = document.getElementById("productCategory");
-
-            productNameHTML.innerHTML = product.name;
-            productDescriptionHTML.innerHTML = product.description;
-            productCostHTML.innerHTML = product.cost + " " + product.currency;
-            vendidosCountHTML.innerHTML = product.soldCount;
-            productCategoryHTML.innerHTML = product.category;
-            relatedProductstoLoad = product.relatedProducts;
-
-            //Muestro las imagenes en forma de galería
-            showImagesGallery(product.images);
-        }
-    });
-
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            commentsArray = resultObj.data;
-            //Muestro comentarios
-            showComments(commentsArray);
-        }
     }
-    )
-});
+}
+
+
+
+    //Función que se ejecuta una vez que se haya lanzado el evento de
+    //que el documento se encuentra cargado, es decir, se encuentran todos los
+    //elementos HTML presentes.
+    document.addEventListener("DOMContentLoaded", function (e) {
+        getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+            if (resultObj.status === "ok") {
+                product = resultObj.data;
+
+                let productNameHTML = document.getElementById("productName");
+                let productDescriptionHTML = document.getElementById("productDescription");
+                let productCostHTML = document.getElementById("productCost");
+                let vendidosCountHTML = document.getElementById("vendidosCount");
+                let productCategoryHTML = document.getElementById("productCategory");
+
+                productNameHTML.innerHTML = product.name;
+                productDescriptionHTML.innerHTML = product.description;
+                productCostHTML.innerHTML = product.cost + " " + product.currency;
+                vendidosCountHTML.innerHTML = product.soldCount;
+                productCategoryHTML.innerHTML = product.category;
+                relatedProductstoLoad = product.relatedProducts;
+
+                //Muestro las imagenes en forma de galería
+                showImagesGallery(product.images);
+            }
+        });
+
+        getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+            if (resultObj.status === "ok") {
+                commentsArray = resultObj.data;
+                //Muestro comentarios
+                showComments(commentsArray);
+            }
+        }
+        )
+    });
 
 //validar comentario
